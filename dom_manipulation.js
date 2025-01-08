33c5c537f8c2f78b6bd7bdd73371ad632774c971
Thomas Steimann <thomas@steimann.ch>
2025-01-08 23:55:21 +0100

    `Added DOM manipulation functionality and CSS styles to index.html and dom_manipulation.js`

--- dom_manipulation.js
+++ dom_manipulation.js
@@ -6,6 +6,14 @@ title.innerHTML = "Hallo Welt";
 
 document.getElementById('test_div').innerHTML = '<p>Test</p>';
 
+document.getElementById('test_div').classList.add('green_bg');
+document.getElementById('test_div').classList.remove('green_bg');
+document.getElementById('test_div').classList.toggle('green_bg');
+
+// document.getElementById('test_input').setAttribute('value', 123);
+document.getElementById('test_input').value = 123;
+console.log(document.getElementById('test_input').value);
+
 // Neuen Button erstellen und ihm eine ID zuweisen
 let button = document.createElement('button');
 button.id = 'myButton';
@@ -13,3 +21,13 @@ button.innerHTML = 'Click Me';
 
 // Button zu einem bestehenden div hinzufügen
 document.getElementById('test_div').appendChild(button);
+
+function toggleDNone(id) {
+    document.getElementById(id).classList.toggle('d_none');
+}
+
+document.getElementById('our_p_tag').addEventListener('click', logger);
+
+function logger() {
+    console.log(555555);
+}
